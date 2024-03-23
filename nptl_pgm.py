d10c966dc8480827ada4ec4701ad98d9511332f3
Viswajith2003 <viswajithviswa715@gmail.com>
2024-03-23 21:30:21 +0530

    practicing pblms

---
+++ nptl_pgm.py
@@ -0,0 +1,18 @@
+# msg1="viswa"
+# msg2=""
+# for i in range(1,1+len(msg1)):
+#   msg2=msg1[-i]+msg2
+# print(msg2)        #viswa
+
+
+# b=[23,44,87.100]
+# a=b[1:]
+# d=b[2:]
+# c=b
+# d[0]=97
+# c[2]=77
+# print(b[2])        #77
+
+
+
+
