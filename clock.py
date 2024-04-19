d1ee3609eb341b888c03329055c818ab11dc9fcf
Viswajith2003 <viswajithviswa715@gmail.com>
2024-04-19 07:13:17 +0530

    q

--- clock.py
+++
@@ -1,8 +0,0 @@
-time=input("Enter time in seconds :")
-time=int(time)
-timeinmin=time/60
-timeinsec=time%60
-timeinhr=timeinmin/60
-timeinmin=timeinmin%60
-print("HH:MM:SS ---- {}:{}:{}".format(timeinhr, timeinmin, timeinsec))
-
