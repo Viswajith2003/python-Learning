7e67f520c55c5ead6a2c5a961b46c7b298fc8c1f
Viswajith2003 <viswajithviswa715@gmail.com>
2024-05-21 23:34:01 +0530

    some changes

--- Workout/largest3.py
+++ Workout/largest3.py
@@ -1,8 +1,8 @@
 a, b, c = map(int, input("Enter the 3 numbers separated by spaces: ").split())
 
 if(a>b and a>c):
-  print(f"{a} is the largest No:")
+  print(f"{a} is the largest Number")
 elif(b>a and b>c):
-  print(f"{b} is the largest No:")
+  print(f"{b} is the largest Number")
 else:
-  print(f"{c} is the largest No:")
+  print(f"{c} is the largest Number")
