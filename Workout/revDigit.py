a116347642817ed1778e9ef070969500ea8b2524
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-07 08:50:39 +0530

    This is the revDigit python pgm

--- Workout/revDigit.py
+++ Workout/revDigit.py
@@ -1,5 +1,8 @@
 n=int(input("Enter the Number: "))
+rev=0
+while(n>0):
+    last=n%10        #Get the last digit
+    rev=rev*10+last  
+    n=int(n/10)      #Remove the last digit
+print(rev)
 
-last=n%2
-print(last)
-digi=n//2
