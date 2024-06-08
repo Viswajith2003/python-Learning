c4fce4453c2aa1a81fb07cf84d7b61cd7af80703
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-08 14:19:55 +0530

    finding error in pgm

--- Workout/revString.py
+++ Workout/revString.py
@@ -1,1 +1,11 @@
 str=input("Enter the string:")
+arr=[]
+try:
+    for i in range(len(str)):
+        str[i]=str[len(str)-i-1]
+        arr=str[i]
+    print(arr)
+except:
+    print("Error occur")
+
+
