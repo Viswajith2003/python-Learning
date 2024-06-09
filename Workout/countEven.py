89281c05b48ac8fd6daf8b3e6aa447d398c20483
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-09 09:25:16 +0530

    finding count and list of even numbers

--- Workout/countEven.py
+++ Workout/countEven.py
@@ -1,10 +1,13 @@
 num=[]
+count=0
 n=int(input("Enter the limit: "))
 for i in range(0,n+1):
     if i%2==0:
         num.append(i)
+    count=count+i
 
-print(num)
+print("The list are: ",num)
+print("No of Elements: ",count)
     
         
         
