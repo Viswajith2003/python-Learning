981a4cf0d92be7fd9eecf478d2514b6b89eda860
Viswajith2003 <viswajithviswa715@gmail.com>
2024-06-09 09:32:03 +0530

    Yes, I got this pblm

--- Workout/countEven.py
+++ Workout/countEven.py
@@ -2,10 +2,9 @@ num=[]
 count=0
 n=int(input("Enter the limit: "))
 for i in range(0,n+1):
-    even=i%2
-    if(even==0):
+    if i%2==0:
         num.append(i)
-        count=count+even
+        count=count+1
 
 print("The list are: ",num)
 print("No of Elements: ",count)
