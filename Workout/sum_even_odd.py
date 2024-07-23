91a50a661e97d4d04080f8d67833b5c1bc6b30e3
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-23 09:06:07 +0530

    start solved sum of odd and even

--- Workout/sum_even_odd.py
+++ Workout/sum_even_odd.py
@@ -1,13 +1,18 @@
 #sum of odd and even numbers
 
 def sum_odd_even(n):
-  sum=0
   n=int(n)
-  for i in range(n):
-    sum=sum+i
-  return(sum)
+  even_sum=0
+  odd_sum=0
+  for i in range(1,n+1):
+    if i%2==0:
+      even_sum=even_sum+i
+    else:
+      odd_sum=odd_sum+i
+  return even_sum,odd_sum
 
 
 
 num=int(input("Enter the number: "))
-print(sum_odd_even(num))
+even,odd=sum_odd_even(num)
+print(odd,even)
