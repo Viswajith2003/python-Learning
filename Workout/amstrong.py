b1995e5bd3ba218ae8c881ea86f8b14b6da9d46c
Viswajith2003 <viswajithviswa715@gmail.com>
2024-07-24 20:49:42 +0530

    amstrong

--- Workout/amstrong.py
+++ Workout/amstrong.py
@@ -1,18 +1,18 @@
 def sum_of_digits(n):
     sum =0
-    for i in range(n):
-      
+    org=n
+    while n>0:
       digit=n%10
       cube=digit**3
       sum=sum+cube
       n=n//10
-      # if sum==n:
-      #   return "yes"
-      # else:
-      #   return "no"
-    return sum
+    if org==sum:
+        return 1
+    else:
+        return 0
+    # return sum
 
 
 
 num = int(input("Enter a number: "))
-print("Sum of square of digits:", sum_of_digits(num))
+print("Sum of cube of digits:", sum_of_digits(num))
