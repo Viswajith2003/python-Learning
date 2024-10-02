c9ab5488726989b174f8265c9284cc54d7a02a31
Viswajith2003 <viswajithviswa715@gmail.com>
2024-10-02 11:30:14 +0530

    set maxElement

--- Workout/Build-Your-Logic/maxEle.py
+++ Workout/Build-Your-Logic/maxEle.py
@@ -1,8 +1,14 @@
 def Maxele(arr):
-    for i in range(len(arr)):
-        for j in range(i+1,len(arr)):
-            if arr[i]<=arr[j]:
-                return arr[j]
+    # for i in range(len(arr)):
+    #     for j in range(i+1,len(arr)):
+    #         if arr[i]<=arr[j]:
+    #             return arr[j]
+    max=0
+    for i in arr:
+        if i>=max:
+            max=i
+    return max
+
 
 
 arr=[5,2,6,1,4,3]
