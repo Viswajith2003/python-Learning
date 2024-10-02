24371cf587599f43e8bcd57581a556d9869e5f34
Viswajith2003 <viswajithviswa715@gmail.com>
2024-10-02 13:06:29 +0530

    set uniq element

---
+++ Workout/Build-Your-Logic/UnqEle.py
@@ -0,0 +1,13 @@
+# Find the Unique Elements in the Given List without using inbuilt Function
+# nums=[1,8,1,2,3,2,4,5,4,5]
+
+def UniqEle(arr):
+   uniq=[]
+   for ele in arr:
+        if ele not in uniq:
+            uniq.append(ele)
+        return uniq
+
+
+arr=[1,8,1,2,3,2,4,5,4,5]
+print(UniqEle(arr))
