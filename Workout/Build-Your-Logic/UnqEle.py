3416434afe306ed7cfe2069d607c812aab575182
Viswajith2003 <viswajithviswa715@gmail.com>
2024-10-02 14:03:05 +0530

    set uniq element

--- Workout/Build-Your-Logic/UnqEle.py
+++ Workout/Build-Your-Logic/UnqEle.py
@@ -2,11 +2,11 @@
 # nums=[1,8,1,2,3,2,4,5,4,5]
 
 def UniqEle(arr):
-   uniq=[]
-   for ele in arr:
+    uniq=[]
+    for ele in arr:
         if ele not in uniq:
             uniq.append(ele)
-        return uniq
+    return uniq
 
 
 arr=[1,8,1,2,3,2,4,5,4,5]
