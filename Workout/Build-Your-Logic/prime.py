345a26e24427cf42f565ec965c07dfc59cfdfdfb
Viswajith2003 <viswajithviswa715@gmail.com>
2024-10-05 14:02:17 +0530

    set prime

--- Workout/Build-Your-Logic/prime.py
+++ Workout/Build-Your-Logic/prime.py
@@ -1,13 +1,26 @@
+
+# flag=0
+# if n<=1:
+#     print("Not prime")
+# for i in range(2,int(n/2)+1):
+#     if n%i==0:
+#         flag=1
+#         break
+# if flag==1:
+#     print("Not prime")
+# else:
+#     print("Prime")
+
+def prime(n):
+    if n<2:
+        return False
+    for j in range(2,int(n/2)+1):
+        if n%j==0:
+            return False
+    return True    
+
 n=int(input("Enter the number :"))
-flag=0
-if n<=1:
-    print("Not prime")
-for i in range(2,int(n/2)+1):
-    if n%i==0:
-        flag=1
-        break
-if flag==1:
-    print("Not prime")
-else:
-    print("Prime")
+for num in range(1,n+1):
+    if prime(num):
+        print(num, "\n")
     
