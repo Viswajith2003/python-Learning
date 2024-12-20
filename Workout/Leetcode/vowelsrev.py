53dbcaec929baaee757809903716e9e6cbcbb114
Viswajith2003 <viswajithviswa715@gmail.com>
2024-12-20 23:37:14 +0530

    vowels rev

--- Workout/Leetcode/vowelsrev.py
+++ Workout/Leetcode/vowelsrev.py
@@ -1,11 +1,27 @@
 def vowels(s):
+    l=0
+    r=len(s)-1
     vowel=["A","E","I","O","U","a","e","i","o","u"]
-    res=[]
-    for i in range(len(s)):
-        if s[i] in vowel:
-            res.append(s[i])
-    rev=res[::-1]
-    return rev
+    s=list(s)
+    while l<r:
+        if s[l] not in vowel:
+            l+=1
+            continue
+
+        if s[r] not in vowel:
+            r-=1
+            continue
+
+        s[l],s[r]=s[r],s[l]
+        l+=1
+        r-=1
+
+    return "".join(s)
+
+
+        
+        
+
     
                 
 s="Leetcode"
