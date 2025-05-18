359c3ca14f60adebc32a84d10798f89b39812918
Viswajith2003 <viswajithviswa715@gmail.com>
2025-05-18 22:46:17 +0530

    rev digit

--- Workout/Technical_coding/revDigit.py
+++ Workout/Technical_coding/revDigit.py
@@ -1,8 +1,42 @@
-n=int(input("Enter the Number: "))
+n=int(input("Enter the number:"))
 rev=0
 while(n>0):
-    last=n%10        #Get the last digit
-    rev=rev*10+last  
-    n=int(n/10)      #Remove the last digit
+    last=n%10
+    rev=rev*10+last
+    n=n//10
 print(rev)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input("Enter the Number: "))
+# rev=0
+# while(n>0):
+#     last=n%10        #Get the last digit
+#     rev=rev*10+last  
+#     n=int(n/10)      #Remove the last digit
+# print(rev)
 
