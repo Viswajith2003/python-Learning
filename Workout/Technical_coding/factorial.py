160f2955783ca48a1678951a2b238f3e9c40eed6
Viswajith2003 <viswajithviswa715@gmail.com>
2025-05-18 22:05:23 +0530

    fact

--- Workout/Technical_coding/factorial.py
+++ Workout/Technical_coding/factorial.py
@@ -1,5 +1,43 @@
-n=int(input("Enter the number: "))
+n=int(input("Enter the number:"))
 fact=1
-for i in range(1,n+1):
-    fact=fact*i
+for i in range(n,0,-1):
+    fact*=i
 print(fact)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# n=int(input("Enter the number: "))
+# fact=1
+# for i in range(1,n+1):
+#     fact=fact*i
+# print(fact)
