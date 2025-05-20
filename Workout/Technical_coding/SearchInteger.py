ce524dc9f83d78a50b45bc526e414be02584cd9d
Viswajith2003 <viswajithviswa715@gmail.com>
2025-05-21 00:27:20 +0530

    searchint

--- Workout/Technical_coding/SearchInteger.py
+++ Workout/Technical_coding/SearchInteger.py
@@ -3,16 +3,68 @@
 # search=4
 # output= 4 found at index 3
 
-def srchInt(nums,srch):
-    for i in range(len(nums)):
-        if nums[i] == srch:
+
+def srchElem(arr,n):
+    for i in range(len(arr)):
+        if arr[i]==n:
             return i
+        
 
-nums=[0,1,2,4,12]
-srch=int(input("Enter the srch element:"))
 
-index=srchInt(nums,srch)
+arr=[0,1,2,4,6]
+n=int(input("Enter the searching element:"))
+index=srchElem(arr,n)
 if index != -1:
-    print(f"{srch} found at index {index}")
+    print(f"{n} is found at index {index}");
 else:
-    print(f"{srch} not found at the list")
+    print("element not found")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def srchInt(nums,srch):
+#     for i in range(len(nums)):
+#         if nums[i] == srch:
+#             return i
+
+# nums=[0,1,2,4,12]
+# srch=int(input("Enter the srch element:"))
+
+# index=srchInt(nums,srch)
+# if index != -1:
+#     print(f"{srch} found at index {index}")
+# else:
+#     print(f"{srch} not found at the list")
