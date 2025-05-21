8eeb7e8156fd3e1e29bb0e48c758c40e6b7779fe
Viswajith2003 <viswajithviswa715@gmail.com>
2025-05-21 22:40:35 +0530

    set even numbers sum

--- Workout/Technical_coding/secondlarge.py
+++ Workout/Technical_coding/secondlarge.py
@@ -1,5 +1,44 @@
-nums=[2,6,5,4,7,3]
-k=2
-n=len(nums)
-nums.sort()
-print("The second largeest element is",nums[n-k])
+arr=[4,2,5,6,1]
+arr.sort()
+print(arr[-2])
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# nums=[2,6,5,4,7,3]
+# k=2
+# n=len(nums)
+# nums.sort()
+# print("The second largeest element is",nums[n-k])
--- Workout/Technical_coding/sum_even_odd.py
+++ Workout/Technical_coding/sum_even_odd.py
@@ -1,18 +1,62 @@
-#sum of odd and even numbers
+# #sum of odd and even numbers
 
-def sum_odd_even(n):
+def sum_of_evenNo(n):
   n=int(n)
+  l=[]
   even_sum=0
-  odd_sum=0
   for i in range(1,n+1):
     if i%2==0:
-      even_sum=even_sum+i
-    else:
-      odd_sum=odd_sum+i
-  return even_sum,odd_sum
+      l.append(i)
+      even_sum+=i
+  return even_sum,l
+
+n=int(input("Enter the number:"))
+even,num=sum_of_evenNo(n)
+print("The numbers are:",num)
+print("sum of even numbers is:",even)
 
 
 
-num=int(input("Enter the number: "))
-even,odd=sum_odd_even(num)
-print(odd,even)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def sum_odd_even(n):
+#   n=int(n)
+#   even_sum=0
+#   odd_sum=0
+#   for i in range(1,n+1):
+#     if i%2==0:
+#       even_sum=even_sum+i
+#     else:
+#       odd_sum=odd_sum+i
+#   return even_sum,odd_sum
+
+
+
+# num=int(input("Enter the number: "))
+# even,odd=sum_odd_even(num)
+# print(odd,even)
