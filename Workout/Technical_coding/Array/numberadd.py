0062fd0dbd846592e72ea2b94b5d62f42dba2470
Viswajith2003 <viswajithviswa715@gmail.com>
2025-06-08 20:04:28 +0530

    increament inteter list of elements

--- Workout/Technical_coding/Array/numberadd.py
+++ Workout/Technical_coding/Array/numberadd.py
@@ -2,15 +2,15 @@ def incOfNum(Arr):
     res=[]
     nums=""
     for i in Arr:
-        nums+=str(i)
-    nums=int(nums)+1
+        nums+=str(i)      #123
+    nums=int(nums)+1      #124    
 
-    while(nums>0):
+    while(nums>0):    
         last=nums%10
         res.append(last)
-        nums=nums//10
+        nums=nums//10     #res=[4,2,1]
     
-    res=res[::-1]
+    res=res[::-1]         #res=[1,2,4]  
     return res
 
 Arr=[1,2,3]
