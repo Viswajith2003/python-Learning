bab6a5f1c1069ba9780527163097b6f2be3fca6a
Viswajith2003 <viswajithviswa715@gmail.com>
2025-06-10 14:54:20 +0530

    Common divisor in string set

--- Workout/Technical_coding/String/commondivisor.py
+++ Workout/Technical_coding/String/commondivisor.py
@@ -1,12 +1,13 @@
-
+import math
 
 def commonDivisor(str1,str2):
-    if (str1+str2)==(str2+str1):
-        length=gcd(len(str1),len(str2))
-        return str1[0:length]
+    if (str1+str2)!=(str2+str1):
+        return " "
+    length=math.gcd(len(str1),len(str2))
+    return str1[0:length]
                           
-    return " "
 
-str1="ababab"
-str2="abab"
+str1="abaaba"
+str2="aba"
 print(commonDivisor(str1,str2))
+
