a4b4eb7229378df3d0cc63502f657276ae5bc602
Viswajith2003 <viswajithviswa715@gmail.com>
2025-06-10 06:09:20 +0530

    merge string slternately

--- Workout/Technical_coding/String/alternativeMerge.py
+++ Workout/Technical_coding/String/alternativeMerge.py
@@ -4,11 +4,14 @@ def alternativeMerge(word1,word2):
     i,j=0,0
     res=[]
     while i<len(word1) and j<len(word2):
-        res.append(word1[i])
+        res.append(word1[i])                    #['a', 'p', 'b', 'q', 'c', 'r']
         res.append(word2[j])
         i+=1
         j+=1
-    return res
+    # res.append(word1[i:])
+    # res.append(word2[j:])
+    return  "".join(res)                        #apbqcr
+    
 
 
 
